4ea7079f8b1b01abffdb1b0a31d29d67ae9949ae
sяva <slavik.bieriezhnoi1995@gmail.com>
2023-11-24 22:59:29 +0200

    add code in files

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,4 +1,32 @@
+// Import library
+import getRandomHexColor from './randomHexColor';
 
-function getRandomHexColor() {
-   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
- }
+//Get button start/stop element
+const dataStartRef = document.querySelector('[data-start]');
+const dataStopRef = document.querySelector('[data-stop]');
+const bodyRef = document.querySelector('body');
+// values
+let timerId = null;
+
+// Add event listener start/stop on btn start/stop
+dataStartRef.addEventListener('click', onStart);
+dataStopRef.addEventListener('click', onStop);
+
+// Function start event listener
+function onStart() {
+  timerId = setInterval(getBgColor, 1000);
+
+  dataStartRef.toggleAttribute('disabled');
+}
+
+// Function stop event listener
+function onStop() {
+  clearInterval(timerId);
+
+  dataStartRef.removeAttribute('disabled');
+}
+
+// Function set random bg color
+function getBgColor() {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+}
